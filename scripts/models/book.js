78d8b83a2ef0308be4e04becb1d6f5557d797b87
Jose Flores <jose.flores1634@gmail.com>
2018-02-27 13:30:12 -0800

    finished step 9

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -26,7 +26,7 @@ var __API_URL__ = 'https://am-jf-booklist.herokuapp.com';
   }
 
   Book.fetchAll = callback =>
-    $.get(`${__API_URL__}/books`)
+    $.get(`${__API_URL__}/api/v1/books`)
       .then(Book.loadAll)
       .then(callback)
       .catch(errorCallback);
