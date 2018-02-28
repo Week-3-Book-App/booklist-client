81d45c04d380cbf4bbfb3d2d084424bdf5537a4f
austincmatteson <austin.c.matteson@gmail.com>
2018-02-28 13:35:26 -0800

    wrong order of routes

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
-page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
-page('/books/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initOneBook));
+page('/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/new', app.bookView.initNewForm);
+page('/books/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initOneBook));
 
 page();
